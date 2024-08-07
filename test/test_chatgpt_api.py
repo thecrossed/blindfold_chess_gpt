3c65dfee333400706cc015100cc1ed3b1e28bb8f
Tianmin Lyu <tianminlyu@gmail.com>
2024-08-07 14:05:53 +0200

    test first prompt

--- test/test_chatgpt_api.py
+++ test/test_chatgpt_api.py
@@ -8,12 +8,28 @@ load_dotenv()
 
 client = OpenAI()
 
-completion = client.chat.completions.create(
-  model="gpt-4o-mini",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
-)
+def get_completion(prompt, model="gpt-4o-mini"):
+    messages = [{"role": "user", "content": prompt}]
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=0
+    )
+    return response.choices[0].message.content
 
-print(completion.choices[0].message)
+
+text = f"""
+d1
+"""
+prompt = f"""
+simplify the text into one chess move notation that stockfish can read. Remember remove the "-" string between the positions.
+
+For example, if I say "move my knight from g1 to f3, the answer is g1f3"
+
+If I say Nf3, you need to figure out the starting square of this piece and combine it into the format such as g1f3. 
+
+If the starting square is ambigous or unclear, you need to ask me what is the starting position of this piece.
+```{text}```
+"""
+response = get_completion(prompt)
+print(response)
