8d8cf4253552e44a302b8b4d6d43d422d582cf30
Tianmin Lyu <tianminlyu@gmail.com>
2024-08-12 15:31:39 +0200

    recording audio

--- test/test_chatgpt_audio.py
+++ test/test_chatgpt_audio.py
@@ -1,6 +1,8 @@
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+from pathlib import Path
+import datetime
 
 # Load environment variables from the .env file (if present)
 # load_dotenv()
@@ -11,17 +13,15 @@ _ = load_dotenv(find_dotenv()) # read local .env file
 OPENAI_API_KEY  = os.getenv('OPENAI_API_KEY')
 client = OpenAI()
 
-audio_file= open("/Users/jasminezhu/blindfold_chess_gpt/resource/test_audio.m4a", "rb")
+# audio to text
+audio_file= open("test/record.m4a", "rb")
 transcription = client.audio.transcriptions.create(
   model="whisper-1", 
   file=audio_file
 )
 print(transcription.text)
 
-from pathlib import Path
-from openai import OpenAI
-client = OpenAI()
-
+# text to audio
 speech_file_path = Path(__file__).parent / "speech.mp3"
 response = client.audio.speech.create(
   model="tts-1",
